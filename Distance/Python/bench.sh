e82f1ba7be5992bfe487f209277477b3f86c0344
Roland Denis <roland.denis@math.univ-lyon1.fr>
2018-11-19 18:34:32 +0100

    Adding general benchmark script for Distance category

--- Distance/Python/bench.sh
+++ Distance/Python/bench.sh
@@ -8,6 +8,14 @@ then
     exit 1
 fi
 
+# Pythran compilation
+if [[ "$2" == Pythran_* ]]
+then
+    cd "$1/$2"
+    make &> /dev/null # Error handling ?
+    cd - > /dev/null
+fi
+
 /usr/bin/time -v python main.py $1 $2 $3 $4 $5 >timing.tmp 2>memory.tmp
 echo Python $1 $2 $3 $4 $5 $(grep "Maximum resident set size" memory.tmp | grep -o "[0-9]*") $(cat timing.tmp)
 
